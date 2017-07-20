65d3ddaf8c8fb6dbdb61e8c6d807223466dc3af0
CDXQ <CDXQ@192.168.0.80>
2017-07-20 18:14:31 +0800

    0720

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -38,25 +38,31 @@ $(function() {
 	}
 
 	var Win = $(window);
-	var iniFontSzie = Util.StorageGetter('font-size');
-	Dom.fiction_container.css('font-size',iniFontSzie);
-	if(!iniFontSzie){
-		iniFontSzie = 14;
+	var RootContainer = Dom.fiction_container;
+	var iniFontSize = Util.StorageGetter('fontSize');
+	if(!iniFontSize){
+		iniFontSize = 14;
 	}
+	Dom.fiction_container.css('font-size',iniFontSize);
 
 	function main(){
 		// todo 整个项目的入口函数
 		EventHanlder();
 		var readerModle = ReaderModel();
-		readerModle.init();
+		var readerUI = ReaderBaseFrame();
+		readerModle.init(function(data){
+			readerUI.parseChapterData(data);
+		});
 	}
 
 	function ReaderModel() {
 		// todo 实现和阅读器相关的数据交互方法
 		var chapter_id;
-		var init = function(){
+		var init = function(UIcallback){
 			getFictionInfo(function(){
-				getCurChapterContent(chapter_id);
+				getCurChapterContent(chapter_id,function(data){
+					UIcallback && UIcallback(data);
+				});				
 			})
 		};
 		var getFictionInfo = function(callback){
@@ -65,12 +71,12 @@ $(function() {
 				callback && callback(chapter_id);
 			},'json')
 		};
-		var getCurChapterContent = function(){
+		var getCurChapterContent = function(chapter_id,callback){
 			$.get('data/data'+chapter_id+'.json',function(data){
 				if(data.result == 0){
 					var url = data.jsonp;
 					Util.getBSONP(url,function(dataJson){
-						console.log(dataJson);
+						callback && callback(dataJson);
 					})
 				}else {
 					console.log('请求状态错误');
@@ -84,6 +90,18 @@ $(function() {
 
 	function ReaderBaseFrame() {
 		// todo 渲染基本的UI结构 
+		function parseChapterData(jsonData){
+			var jsonObj = JSON.parse(jsonData);
+			var html = "<h4>" +jsonObj.t+ "</h4>";
+			for(var i = 0;i<jsonObj.p.length;i++){
+				html += "<p>" +jsonObj.p[i]+ "</p>";
+			}
+			return html;
+			
+		}
+		return function(container){
+			container.html(html);
+		};
 	}
 
 	function EventHanlder() {
@@ -109,26 +127,26 @@ $(function() {
 			}
 		})
 		$('#large-font').click(function(){
-			if(iniFontSzie > 20){
+			if(iniFontSize > 20){
 				return;
 			}
-			iniFontSzie += 1;
-			Dom.fiction_container.css('font-size',iniFontSzie);
+			iniFontSize += 1;
+			Dom.fiction_container.css('font-size',iniFontSize);
 		})
 	    $('#small-font').click(function(){
-	    	if(iniFontSzie < 12){
+	    	if(iniFontSize < 12){
 				return;
 			}
-	    	iniFontSzie -= 1;
-	    	Dom.fiction_container.css('font-size',iniFontSzie);
-	    	Util.StorageSetter('font-size',iniFontSzie);
+	    	iniFontSize -= 1;
+	    	Dom.fiction_container.css('font-size',iniFontSize);
+	    	Util.StorageSetter('fontSize',iniFontSize);
 		})
 		Win.scroll(function(){
 			Dom.top_nav.hide();
 			Dom.bottom_nav.hide();
 			Dom.font_container.hide();
 			Dom.font_button.removeClass('current');
-			Util.StorageSetter('font-size',iniFontSzie);
+			Util.StorageSetter('fontSize',iniFontSize);
 		})
 	}
 	main();
